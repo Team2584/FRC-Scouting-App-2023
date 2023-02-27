ea201c3fb17523a6111782654d90ee57a185828c
AJ-Williams1 <65444874+AJ-Williams1@users.noreply.github.com>
2023-02-27 08:34:22 +0000

    Refresh environment variables after installing

--- install.ps1
+++ install.ps1
@@ -2,6 +2,17 @@ Invoke-WebRequest -OutFile nvm-setup.exe https://github.com/coreybutler/nvm-wind
 .\nvm-setup.exe
 $nid = (Get-Process nvm-setup).id
 Wait-Process $nid
+
+foreach($level in "Machine","User") {
+   [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
+      # For Path variables, append the new values, if they're not already in there
+      if($_.Name -match 'Path$') { 
+         $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
+      }
+      $_
+   } | Set-Content -Path { "Env:$($_.Name)" }
+}
+
 nvm install latest
 nvm use latest
 Invoke-WebRequest -OutFile FRC-Scouting-App-2023-main.zip https://github.com/Team2584/FRC-Scouting-App-2023/archive/refs/heads/main.zip
